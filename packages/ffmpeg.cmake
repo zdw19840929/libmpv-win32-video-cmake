19384b0889f63f2f3cde59ee31f50209c504826b
zdw19840929 <15810768024@163.com>
2026-09-22 19:53:30 +0800

    Update ffmpeg.cmake

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -1,3 +1,5 @@
+# Broad playback support; Vulkan rendering is configured separately in mpv/libplacebo.
+# GPL is enabled, matching the supplied configuration.
 ExternalProject_Add(ffmpeg
         DEPENDS
         amf-headers
@@ -34,6 +36,7 @@ ExternalProject_Add(ffmpeg
         openal-soft
     GIT_REPOSITORY https://github.com/FFmpeg/FFmpeg.git
     SOURCE_DIR ${SOURCE_LOCATION}
+    GIT_TAG release/7.1
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
     GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !tests/ref/fate"
     PATCH_COMMAND ${EXEC} git apply ${CMAKE_CURRENT_SOURCE_DIR}/ffmpeg-*.patch
@@ -56,10 +59,10 @@ ExternalProject_Add(ffmpeg
         --enable-stripping
 
         --disable-muxers
-        --disable-decoders
+        --enable-decoders
         --disable-encoders
-        --disable-demuxers
-        --disable-parsers
+        --enable-demuxers
+        --enable-parsers
         --disable-protocols
         --disable-filters
         --disable-doc
@@ -72,7 +75,6 @@ ExternalProject_Add(ffmpeg
 
         --enable-amf
         --enable-dxva2
-        --enable-libmfx
         --enable-d3d11va
 
         --disable-vaapi
@@ -121,153 +123,6 @@ ExternalProject_Add(ffmpeg
         --enable-avdevice
         --enable-swscale
         --enable-swresample
-
-        --enable-decoder=flv
-        --enable-decoder=h263
-        --enable-decoder=h263i
-        --enable-decoder=h263p
-        --enable-decoder=h264*
-        --enable-decoder=mpeg1video
-        --enable-decoder=mpeg2*
-        --enable-decoder=mpeg4*
-        --enable-decoder=vp6
-        --enable-decoder=vp6a
-        --enable-decoder=vp6f
-        --enable-decoder=vp8*
-        --enable-decoder=vp9*
-        --enable-decoder=hevc*
-        --enable-decoder=av1*
-        --enable-decoder=libdav1d
-        --enable-decoder=theora
-        --enable-decoder=msmpeg*
-        --enable-decoder=mjpeg*
-        --enable-decoder=wmv*
-
-        --enable-decoder=aac*
-        --enable-decoder=ac3
-        --enable-decoder=alac
-        --enable-decoder=als
-        --enable-decoder=ape
-        --enable-decoder=atrac*
-        --enable-decoder=eac3
-        --enable-decoder=flac
-        --enable-decoder=gsm*
-        --enable-decoder=mp1*
-        --enable-decoder=mp2*
-        --enable-decoder=mp3*
-        --enable-decoder=mpc*
-        --enable-decoder=opus
-        --enable-decoder=ra*
-        --enable-decoder=ralf
-        --enable-decoder=shorten
-        --enable-decoder=tak
-        --enable-decoder=tta
-        --enable-decoder=vorbis
-        --enable-decoder=wavpack
-        --enable-decoder=wma*
-        --enable-decoder=pcm*
-        --enable-decoder=dsd*
-        --enable-decoder=dca
-
-        --enable-decoder=ssa
-        --enable-decoder=ass
-        --enable-decoder=dvbsub
-        --enable-decoder=dvdsub
-        --enable-decoder=srt
-        --enable-decoder=stl
-        --enable-decoder=subrip
-        --enable-decoder=subviewer
-        --enable-decoder=subviewer1
-        --enable-decoder=text
-        --enable-decoder=vplayer
-        --enable-decoder=webvtt
-        --enable-decoder=movtext
-
-        --enable-decoder=mjpeg
-        --enable-decoder=ljpeg
-        --enable-decoder=jpegls
-        --enable-decoder=jpeg2000
-        --enable-decoder=png
-        --enable-decoder=gif
-        --enable-decoder=bmp
-        --enable-decoder=tiff
-        --enable-decoder=webp
-        --enable-decoder=jpegls
-
-        --enable-demuxer=concat
-        --enable-demuxer=data
-        --enable-demuxer=flv
-        --enable-demuxer=hls
-        --enable-demuxer=latm
-        --enable-demuxer=live_flv
-        --enable-demuxer=loas
-        --enable-demuxer=m4v
-        --enable-demuxer=mov
-        --enable-demuxer=mpegps
-        --enable-demuxer=mpegts
-        --enable-demuxer=mpegvideo
-        --enable-demuxer=hevc
-        --enable-demuxer=rtsp
-        --enable-demuxer=mpeg4
-        --enable-demuxer=mjpeg*
-        --enable-demuxer=avi
-        --enable-demuxer=av1
-        --enable-demuxer=matroska
-        --enable-demuxer=dash
-        --enable-demuxer=webm_dash_manifest
-
-        --enable-demuxer=aac
-        --enable-demuxer=ac3
-        --enable-demuxer=aiff
-        --enable-demuxer=ape
-        --enable-demuxer=asf
-        --enable-demuxer=au
-        --enable-demuxer=avi
-        --enable-demuxer=flac
-        --enable-demuxer=flv
-        --enable-demuxer=matroska
-        --enable-demuxer=mov
-        --enable-demuxer=m4v
-        --enable-demuxer=mp3
-        --enable-demuxer=mpc*
-        --enable-demuxer=ogg
-        --enable-demuxer=pcm*
-        --enable-demuxer=rm
-        --enable-demuxer=shorten
-        --enable-demuxer=tak
-        --enable-demuxer=tta
-        --enable-demuxer=wav
-        --enable-demuxer=wv
-        --enable-demuxer=xwma
-        --enable-demuxer=dsf
-        --enable-demuxer=truehd
-        --enable-demuxer=dts
-        --enable-demuxer=dtshd
-
-        --enable-demuxer=ass
-        --enable-demuxer=srt
-        --enable-demuxer=stl
-        --enable-demuxer=webvtt
-        --enable-demuxer=subviewer
-        --enable-demuxer=subviewer1
-        --enable-demuxer=vplayer
-
-        --enable-parser=h263
-        --enable-parser=h264
-        --enable-parser=hevc
-        --enable-parser=mpeg4
-        --enable-parser=mpeg4video
-        --enable-parser=mpegvideo
-
-        --enable-parser=aac*
-        --enable-parser=ac3
-        --enable-parser=cook
-        --enable-parser=flac
-        --enable-parser=gsm
-        --enable-parser=mpegaudio
-        --enable-parser=tak
-        --enable-parser=vorbis
-        --enable-parser=dca
 
         --enable-filter=overlay
         --enable-filter=equalizer
