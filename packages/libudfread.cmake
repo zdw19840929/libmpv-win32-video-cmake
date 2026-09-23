0e9d836f5993a875529bfd885e63d61273ce094f
zdw19840929 <15810768024@163.com>
2026-09-23 09:04:41 +0800

    Update libudfread.cmake

--- packages/libudfread.cmake
+++ packages/libudfread.cmake
@@ -1,17 +1,15 @@
 ExternalProject_Add(libudfread
-    GIT_REPOSITORY https://code.videolan.org/videolan/libudfread.git
-    SOURCE_DIR ${SOURCE_LOCATION}
-    GIT_CLONE_FLAGS "--filter=tree:0"
+    URL https://download.videolan.org/pub/videolan/libudfread/libudfread-1.1.2.tar.bz2
+    URL_HASH SHA256=d5946f19b27288ce0911eb11fe583c136767421220ece5fba3fc96245efd208915
+    DOWNLOAD_DIR ${SOURCE_LOCATION}
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} autoreconf -fi && CONF=1 <SOURCE_DIR>/configure
+    CONFIGURE_COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
-    BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-force_rebuild_git(libudfread)
 cleanup(libudfread install)
