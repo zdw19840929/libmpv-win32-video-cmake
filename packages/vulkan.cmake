cc9ad2d81f664f645dd07ab5f27193571576ae74
zdw19840929 <15810768024@163.com>
2026-09-23 08:48:34 +0800

    Update vulkan.cmake

--- packages/vulkan.cmake
+++ packages/vulkan.cmake
@@ -5,8 +5,9 @@ ExternalProject_Add(vulkan
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
     GIT_REMOTE_NAME origin
-    GIT_TAG main
-    PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/vulkan-*.patch
+    GIT_TAG v1.3.296
+    GIT_RESET v1.3.296
+    PATCH_COMMAND ${EXEC} git apply ${CMAKE_CURRENT_SOURCE_DIR}/vulkan-0001-cross-compile-static-linking-hacks.patch
     CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -G Ninja
         -DCMAKE_BUILD_TYPE=Release
