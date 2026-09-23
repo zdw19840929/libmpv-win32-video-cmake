85aa325ee49c308e43188dbc81f538cc56cb7b2c
zdw19840929 <15810768024@163.com>
2026-09-23 09:04:09 +0800

    Update libdvdread.cmake

--- packages/libdvdread.cmake
+++ packages/libdvdread.cmake
@@ -1,19 +1,17 @@
 ExternalProject_Add(libdvdread
     DEPENDS libdvdcss
-    GIT_REPOSITORY https://code.videolan.org/videolan/libdvdread.git
-    SOURCE_DIR ${SOURCE_LOCATION}
-    GIT_CLONE_FLAGS "--filter=tree:0"
+    URL https://download.videolan.org/pub/videolan/libdvdread/6.1.3/libdvdread-6.1.3.tar.bz2
+    URL_HASH SHA256=ce35454997a208cbe50e91232f0e73fb1ac3471965813a13b8730a8f18a15369
+    DOWNLOAD_DIR ${SOURCE_LOCATION}
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} autoreconf -fi && CONF=1 <SOURCE_DIR>/configure
+    CONFIGURE_COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
         --with-libdvdcss
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
-    BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-force_rebuild_git(libdvdread)
 cleanup(libdvdread install)
