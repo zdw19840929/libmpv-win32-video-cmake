b2b2446c44a106276a92f602857cd09f02b8657a
zdw19840929 <15810768024@163.com>
2026-09-23 20:33:57 +0800

    Update llvm-libcxx.cmake

--- toolchain/llvm/llvm-libcxx.cmake
+++ toolchain/llvm/llvm-libcxx.cmake
@@ -7,6 +7,9 @@ ExternalProject_Add(llvm-libcxx
     PATCH_COMMAND ${CMAKE_COMMAND}
         "-DLLVM_SOURCE_DIR=<SOURCE_DIR>"
         -P "${CMAKE_CURRENT_LIST_DIR}/fix-libcxx-locale.cmake"
+        COMMAND sed -i
+            "s/return 4 \\/\\* ExceptionExecuteHandler in mingw \\*\\//return static_cast<EXCEPTION_DISPOSITION>(4) \\/\\* ExceptionExecuteHandler in mingw \\*\\//g"
+            <SOURCE_DIR>/libunwind/src/Unwind-seh.cpp
     LIST_SEPARATOR ,
     CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR>/runtimes -B<BINARY_DIR>
         -G Ninja
