3f7a0321afc44ba9217f22e906b4b3676096e8b0
zdw19840929 <15810768024@163.com>
2026-09-23 22:22:55 +0800

    Update winpthreads.cmake

--- toolchain/winpthreads.cmake
+++ toolchain/winpthreads.cmake
@@ -10,8 +10,11 @@ ExternalProject_Add(winpthreads
         --disable-shared
         --enable-static
     BUILD_COMMAND ${MAKE} LTO=0 GC=0
-    INSTALL_COMMAND ${MAKE} LTO=0 GC=0 install-strip
-    LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+    INSTALL_COMMAND ${MAKE} LTO=0 GC=0 install
+    BUILD_IN_SOURCE 1
+    LOG_CONFIGURE 1
+    LOG_BUILD 1
+    LOG_INSTALL 1
 )
 
 cleanup(winpthreads install)
