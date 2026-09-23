37cd50fc7e0eb39ec02c1e62050c2a09da76482e
zdw19840929 <15810768024@163.com>
2026-09-23 09:29:07 +0800

    Fix URL_HASH and format LOG options in libudfread

--- packages/libudfread.cmake
+++ packages/libudfread.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(libudfread
     URL https://download.videolan.org/pub/videolan/libudfread/libudfread-1.1.2.tar.bz2
-    URL_HASH SHA256=d5946f19b27288ce0911eb11fe583c136767421220ece5fba3fc96245efd208915
+    URL_HASH SHA256=d5946f19b27288ce0911eb11fe583c136767421220ece3bafc96245efd208915
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/configure
@@ -9,7 +9,11 @@ ExternalProject_Add(libudfread
         --disable-shared
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
-    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+    LOG_DOWNLOAD 1
+    LOG_UPDATE 1
+    LOG_CONFIGURE 1
+    LOG_BUILD 1
+    LOG_INSTALL 1
 )
 
 cleanup(libudfread install)
