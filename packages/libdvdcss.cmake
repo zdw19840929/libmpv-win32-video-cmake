9ea374f11ba6618baaabcdf1063a682e15214f8b
zdw19840929 <15810768024@163.com>
2026-09-23 08:30:27 +0800

    Update libdvdcss.cmake

--- packages/libdvdcss.cmake
+++ packages/libdvdcss.cmake
@@ -1,18 +1,17 @@
 ExternalProject_Add(libdvdcss
-    GIT_REPOSITORY https://code.videolan.org/videolan/libdvdcss.git
-    SOURCE_DIR ${SOURCE_LOCATION}
-    GIT_CLONE_FLAGS "--filter=tree:0"
+    URL "https://download.videolan.org/pub/libdvdcss/1.4.3/libdvdcss-1.4.3.tar.bz2"
+    URL_HASH SHA256=233cc92f5dc01c5d3a96f5b3582be7d5cee5a35a52d3a08158745d3d86070079
+    DOWNLOAD_DIR ${SOURCE_LOCATION}
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} autoreconf -fi && CONF=1 <SOURCE_DIR>/configure
+    CONFIGURE_COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
+        --enable-static
         --disable-doc
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
-    BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-force_rebuild_git(libdvdcss)
 cleanup(libdvdcss install)
